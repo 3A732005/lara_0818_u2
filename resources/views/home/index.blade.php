26abfe0d22ba529fed0319f2a9bff6f3209b537d
li ya hsuan <ab53014698@yahoo.com.tw>
2020-10-25 20:33:42 +0800

    index.php改為index.blade.php+master.blade.php+navigation.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,13 @@
     <title>Title</title>
 </head>
 <body>
+    @extends('layouts.master')
+
+    @section('title','Home')
+
+    @section('content')
     <h1>Home</h1>
     <p>This is my body content.</p>
+    @endsection
 </body>
 </html>
